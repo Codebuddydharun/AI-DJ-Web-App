89b08a661205d78c5dbb7def23fddd9702993ffd
Codebuddydharun <83393251+Codebuddydharun@users.noreply.github.com>
2022-02-17 13:46:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftwristx=0;
 leftwristy=0;
 rightwristx=0;
 rightwristy=0;
+scoreleftwrist=0;
+
 
 function preload(){
     song=loadSound("music.mp3");
@@ -19,9 +21,7 @@ function setup(){
     model.on('pose', gotposes);
 }
 
-function draw(){
-    image(video,0,0,600,425);
-}
+
 
 function modelloaded(){
 console.log("Model loaded");
@@ -38,6 +38,9 @@ function gotposes(results){
         rightwristx=results[0].pose.rightWrist.x;
         rightwristy=results[0].pose.rightWrist.y;
         console.log(rightwristx+"rightwristx"+rightwristy+"rightwristy");
+
+        scoreleftwrist=results[0].pose.keypoints[9].score;
+        console.log("scoreleftwrist-"+scoreleftwrist);
     }
 }
 
@@ -45,4 +48,18 @@ function play(){
     song.play();
     song.rate(1);
     song.setVolume(1);
+}
+
+function draw(){
+    image(video,0,0,600,425);
+    fill("#ff0000");
+    stroke("#ff0000");
+    if(scoreleftwrist>0.2){
+        circle(leftwristx,leftwristy,20);
+        a=Number(leftwristy);
+        b=floor(a);
+        volume=b/425;
+        document.getElementById("volume").innerHTML="volume:"+volume;
+        song.setVolume(volume);
+    }
 }
